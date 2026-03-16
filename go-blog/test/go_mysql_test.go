267da988303232b2e3688723ff1659f8ba6c9f25
sunlong <ls00330@gdu-tech.com>
2026-03-16 15:20:51 +0800

    init

---
+++ go-blog/test/go_mysql_test.go
@@ -0,0 +1,18 @@
+package test
+
+import (
+	"fmt"
+	"go-blog/util"
+	"testing"
+)
+
+// 测试类标准写法
+func TestMysqlConnect(t *testing.T) {
+	err := util.CreateTestDB(".env_prod")
+	if err != nil {
+		t.Fatalf("数据库连接失败: %v", err)
+	}
+
+	fmt.Println("数据库连接成功！")
+
+}
